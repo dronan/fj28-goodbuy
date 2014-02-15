31fc09d380af0f55b1f64168c443b73bc99eacfd
dronan <dronan@gmail.com>
2014-02-15 18:13:15 -0200

    crud with hibernate

--- src/br/com/caelum/goodbuy/testes/AlteracaoDeProduto.java
+++ src/br/com/caelum/goodbuy/testes/AlteracaoDeProduto.java
@@ -4,27 +4,26 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
+
 import br.com.caelum.goodbuy.modelo.Produto;
 
-@SuppressWarnings("deprecation")
-public class AdicaoDeProduto {
+public class AlteracaoDeProduto {
+
 	public static void main(String[] args) {
 
 		AnnotationConfiguration configuration = new AnnotationConfiguration();
 		configuration.configure();
-
+		
 		SessionFactory factory = configuration.buildSessionFactory();
 		Session session = factory.openSession();
-
-		Produto produto = new Produto();
-
-		produto.setNome("Cadeira");
-		produto.setDescricao("Uma cadeira qualquer");
-		produto.setPreco(5.0);
-
+		
+		Produto produto = (Produto) session.load(Produto.class, 1L);
+		
 		Transaction tx = session.beginTransaction();
-		session.save(produto);
+		produto.setPreco(500.0);
+		session.update(produto);
 		tx.commit();
-
+		
 	}
+
 }
--- src/br/com/caelum/goodbuy/testes/RemocaoDeProdutos.java
+++ src/br/com/caelum/goodbuy/testes/RemocaoDeProdutos.java
@@ -4,27 +4,24 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
+
 import br.com.caelum.goodbuy.modelo.Produto;
 
-@SuppressWarnings("deprecation")
-public class AdicaoDeProduto {
+public class RemocaoDeProdutos {
 	public static void main(String[] args) {
-
+		
 		AnnotationConfiguration configuration = new AnnotationConfiguration();
 		configuration.configure();
-
+		
 		SessionFactory factory = configuration.buildSessionFactory();
 		Session session = factory.openSession();
-
-		Produto produto = new Produto();
-
-		produto.setNome("Cadeira");
-		produto.setDescricao("Uma cadeira qualquer");
-		produto.setPreco(5.0);
-
+		
+		Produto produto = (Produto) session.load(Produto.class, 2L);
+		
 		Transaction tx = session.beginTransaction();
-		session.save(produto);
+		session.delete(produto);
 		tx.commit();
-
+		
+		
 	}
 }
