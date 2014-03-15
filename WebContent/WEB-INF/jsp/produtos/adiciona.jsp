da0f4b21994b490c858abd7eb9cf160c7653cd37
dronan <dronan@gmail.com>
2014-03-15 18:11:11 -0300

    add products logic

---
+++ WebContent/WEB-INF/jsp/produtos/adiciona.jsp
@@ -0,0 +1,1 @@
+<h3>Produto Adicionado com Suceso!</h3>
--- src/br/com/caelum/goodbuy/controller/ProdutosController.java
+++ src/br/com/caelum/goodbuy/controller/ProdutosController.java
@@ -5,17 +5,29 @@ import java.util.List;
 import br.com.caelum.goodbuy.dao.ProdutoDao;
 import br.com.caelum.goodbuy.modelo.Produto;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 
 @Resource
 public class ProdutosController {
 
 	private final ProdutoDao dao;
+	private final Result result;
 
-	public ProdutosController(ProdutoDao dao) {
+	public ProdutosController(ProdutoDao dao, Result result) {
 		this.dao = dao;
+		this.result = result;
 	}
 
 	public List<Produto> lista() {
 		return dao.listaTudo();
+	}
+
+	public void adiciona(Produto produto) {
+		dao.salva(produto);
+		//result.redirectTo(ProdutosController.class).lista();
+		result.redirectTo(this).lista();
+	}
+
+	public void formulario() {
 	}
 }
