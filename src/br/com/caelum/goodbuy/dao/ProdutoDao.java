130f2c9448b95d64e1a964b37e8e4c4a51538a51
dronan <dronan@gmail.com>
2014-03-15 15:56:32 -0300

    dependency / products list

--- src/br/com/caelum/goodbuy/dao/ProdutoDao.java
+++ src/br/com/caelum/goodbuy/dao/ProdutoDao.java
@@ -1,18 +1,23 @@
 package br.com.caelum.goodbuy.dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
 import br.com.caelum.goodbuy.infra.CriadorDeSession;
 import br.com.caelum.goodbuy.modelo.Produto;
+import br.com.caelum.vraptor.ioc.Component;
 
+@Component
 public class ProdutoDao {
 
 	private final Session session;
-	
+
 	public ProdutoDao() {
 		this.session = CriadorDeSession.getSession();
 	}
-	
+
 	public void salva(Produto produto) {
 		Transaction tx = session.beginTransaction();
 		session.save(produto);
@@ -26,10 +31,13 @@ public class ProdutoDao {
 		tx.commit();
 	}
 
-	public  void remove(Produto produto) {
+	public void remove(Produto produto) {
 		Transaction tx = session.beginTransaction();
 		session.delete(produto);
 		tx.commit();
 	}
 
+	public List<Produto> listaTudo() {
+		return this.session.createCriteria(Produto.class).list();
+	}
 }
