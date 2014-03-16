a8233edbeefb1927b6ae902a8948841a3fe44a22
dronan <dronan@gmail.com>
2014-03-16 15:17:33 -0300

    refactoring session construction

--- src/br/com/caelum/goodbuy/infra/CriadorDeSession.java
+++ src/br/com/caelum/goodbuy/infra/CriadorDeSession.java
@@ -1,5 +1,8 @@
 package br.com.caelum.goodbuy.infra;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -10,14 +13,24 @@ import br.com.caelum.vraptor.ioc.ComponentFactory;
 public class CriadorDeSession implements ComponentFactory<Session> {
 
 	private final SessionFactory factory;
+	private Session session;
 
 	public CriadorDeSession(SessionFactory factory) {
 		this.factory = factory;
 	}
 
+	@PostConstruct
+	public void abre(){
+		this.session = factory.openSession();
+	}
+	
 	public Session getInstance() {
-		Session session = factory.openSession();
-		return session;
+		return this.session;
+	}
+	
+	@PreDestroy
+	public void fecha(){
+		this.session.close();
 	}
 
 }
--- src/br/com/caelum/goodbuy/infra/CriadorDeSessionFactory.java
+++ src/br/com/caelum/goodbuy/infra/CriadorDeSessionFactory.java
@@ -1,20 +1,37 @@
 package br.com.caelum.goodbuy.infra;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.ComponentFactory;
 
 @Component
+@ApplicationScoped
 public class CriadorDeSessionFactory implements
 		ComponentFactory<SessionFactory> {
-
-	public SessionFactory getInstance() {
+	
+	private SessionFactory factory;
+	
+	@PostConstruct
+	public void abre(){
 		Configuration configuration = new Configuration()
-				.configure();
-		SessionFactory factory = configuration.buildSessionFactory();
-		return factory;
+		.configure();
+		this.factory = configuration.buildSessionFactory();
+
+	}
+	
+	public SessionFactory getInstance() {
+		return this.factory;
+	}
+	
+	@PreDestroy
+	public void fecha(){
+		this.factory.close();
 	}
 
 }
