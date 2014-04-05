3fa09a283b2f1c4078f6e5aecf401cd477e3be89
dronan <dronan@gmail.com>
2014-04-05 16:20:02 -0300

    vraptor validator

--- WebContent/WEB-INF/jsp/produtos/formulario.jsp
+++ WebContent/WEB-INF/jsp/produtos/formulario.jsp
@@ -11,10 +11,10 @@
 		<fieldset>
 			<legend>Adicionar Produto</legend>
 			<label for="nome">Nome:</label> <input id="nome" type="text"
-				name="produto.nome" /> <label for="descricao">Descricao:</label>
-			<textarea id="descricao" name="produto.descricao"></textarea>
+				name="produto.nome" value="${produto.nome }" /> <label for="descricao">Descricao:</label>
+			<textarea id="descricao" name="produto.descricao">${produto.descricao }</textarea>
 			<label for="preco">Preco:</label> <input id="preco"
-				name="produto.preco" type="text" />
+				name="produto.preco" type="text" value="${produto.preco }" />
 			<button type="submit">Enviar</button>
 		</fieldset>
 	</form>
--- src/br/com/caelum/goodbuy/controller/ProdutosController.java
+++ src/br/com/caelum/goodbuy/controller/ProdutosController.java
@@ -6,16 +6,21 @@ import br.com.caelum.goodbuy.dao.ProdutoDao;
 import br.com.caelum.goodbuy.modelo.Produto;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.Validator;
+import br.com.caelum.vraptor.validator.Message;
+import br.com.caelum.vraptor.validator.ValidationMessage;
 
 @Resource
 public class ProdutosController {
 
 	private final ProdutoDao dao;
 	private final Result result;
+	private final Validator validator;
 
-	public ProdutosController(ProdutoDao dao, Result result) {
+	public ProdutosController(ProdutoDao dao, Result result, Validator validator) {
 		this.dao = dao;
 		this.result = result;
+		this.validator = validator;
 	}
 
 	public List<Produto> lista() {
@@ -23,11 +28,29 @@ public class ProdutosController {
 	}
 
 	public void adiciona(Produto produto) {
+		valida(produto);
+		validator.onErrorUsePageOf(ProdutosController.class).formulario();
 		dao.salva(produto);
-		// result.redirectTo(ProdutosController.class).lista();
 		result.redirectTo(this).lista();
 	}
 
+	public void valida(Produto produto){
+
+		if (produto.getNome() == null || produto.getNome().length() < 3){
+			validator.add(new ValidationMessage("Nome é obrigatório e precisa ter mais de 3 letras", "produto.nome"));
+		}
+		
+		if (produto.getDescricao() == null || produto.getDescricao().length() > 40){
+			validator.add(new ValidationMessage("Obrigação é obrigatória e não pode ter mais de 40 caracteres", "produto.descricao"));
+		}
+		
+		if (produto.getPreco() == null || produto.getPreco() <= 0){
+			validator.add(new ValidationMessage("Preço precisa ser positivo", "produto.preco"));
+		}
+		
+		
+	}
+	
 	public void formulario() {
 	}
 
@@ -36,11 +59,13 @@ public class ProdutosController {
 	}
 
 	public void altera(Produto produto) {
+		valida(produto);
+		validator.onErrorUsePageOf(ProdutosController.class).edita(produto.getId());
 		dao.atualiza(produto);
 		result.redirectTo(this).lista();
 	}
-	
-	public void remove(Long id){
+
+	public void remove(Long id) {
 		Produto produto = dao.carrega(id);
 		dao.remove(produto);
 		result.redirectTo(this).lista();
